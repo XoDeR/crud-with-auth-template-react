126bd3f6f3c11991a9a8019f7c22382ce670d99a
XoDeR <xoder.vs@gmail.com>
2025-08-27 16:45:10 +0200

    minor formatting typo

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import useAuthStore from "../stores/authStore";
 
 const ProtectedRoute = () => {
-  const token = useAuthStore((state) =>state.token);
+  const token = useAuthStore((state) => state.token);
 
   if (!token) {
     return <Navigate to="/login"/>
